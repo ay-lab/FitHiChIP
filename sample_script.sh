7bfca4f6f9b5ff6fda58653ba855d9de3f8e57b1
souryalji <sourya.bhatta@gmail.com>
2017-11-27 10:57:20 -0800

    Updated sample execution script

--- sample_script.sh
+++ sample_script.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# executing FitHiChIP without any bias correction
+# executing FitHiChIP according to the settings of condifuration file
+# any modification of input parameters should be done via the configuration file 
 ./FitHiChIP_HiCPro.sh -C configfile
 
-# executing FitHiChIP with bias correction
-# -b 1 means that interactions having bias values within the default thresholds (0.2 and 5) will be only considered as an input to FitHiC
-# -e 1 means that probability of an interaction (computed using FitHiC) will be multiplied with their bias values
-# accordingly, computation of the P values will be adjusted
-./FitHiChIP_HiCPro.sh -C configfile -b 1 -e 1
 
