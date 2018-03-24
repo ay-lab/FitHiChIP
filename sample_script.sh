b741e4b8cf9d10d55475c43bc24e03d00860cacf
souryalji <sourya.bhatta@gmail.com>
2018-03-24 02:38:21 -0700

    Updated sample script

--- sample_script.sh
+++ sample_script.sh
@@ -1,7 +1,21 @@
-#!/bin/bash
+#!/bin/bash -ex
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=10GB
+#PBS -l walltime=48:00:00
+#PBS -m ae
+#PBS -j eo
+#PBS -V
+source ~/.bashrc
+#source ~/.bash_profile
+hostname
+TMPDIR=/scratch
+cd $PBS_O_WORKDIR
 
 # executing FitHiChIP according to the settings of condifuration file
 # any modification of input parameters should be done via the configuration file 
-./FitHiChIP_HiCPro.sh -C configfile
+#./FitHiChIP_HiCPro.sh -C configfile_BiasCorrection_CoverageBias
+#./FitHiChIP_HiCPro.sh -C configfile_BiasCorrection_ICEBias
+./FitHiChIP_HiCPro.sh -C configfile_P2P_BiasCorrection_CoverageBias
+#./FitHiChIP_HiCPro.sh -C configfile_P2P_BiasCorrection_ICEBias
 
 
